902a28d7fd1897ce11ea888242d1ff6c53dd9bec
Kevin Willot <kevin.willot.nawezi@gmail.com>
2021-06-09 20:40:23 +0200

    update menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -23,6 +23,13 @@ export class MenuComponent implements OnInit, AfterContentInit {
         }
       });
     })();
+    const hamburger = document.querySelector('.hamburger');
+    // On click
+    hamburger.addEventListener('click', function () {
+      // Toggle class "is-active"
+      hamburger.classList.toggle('is-active');
+      // Do something else, like open/close menu
+    });
   }
 
   goToHomeElement(elemId): void {
